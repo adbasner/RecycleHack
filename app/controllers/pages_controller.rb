19b5b6bb475c8eeb928645b1896d71b7e9dc079e
Andrew Basner <andrewbasner@gmail.com>
2019-06-12 20:52:16 -0500

    Complete with no search function

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,25 @@ class PagesController < ApplicationController
   def index
     response = Unirest.get('https://raw.githubusercontent.com/chicagoruby/hack-night-is-it-recyclable/master/data/chicago-recycling-guide.json')
     @trash = response.body["data"]
-    # puts @trash
+    # @single_item = []
+    # @trash = []
+    # if params['search']== ""  
+    #   @single_item = response.body["data"]
+    #   puts "no search term"
+    # else
+    #   response.body["data"].each do |i|
+    #     if i["Material"].downcase  == params['search'].downcase 
+    #       @trash.push(i)
+    #       puts @trash
+    #       puts 'found'
+    #       break
+    #     else
+    #       puts @trash
+    #       puts "not found"
+    #     end
+    #   end
+    # end
+
     render 'index.html.erb'
   end
   
