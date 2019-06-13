1e63972650584ae840e487a11900a54986c06239
Andrew Basner <andrewbasner@gmail.com>
2019-06-13 03:16:45 -0500

    make basic add form

--- app/controllers/trash_controller.rb
+++ app/controllers/trash_controller.rb
@@ -1,11 +1,32 @@
 class TrashController < ApplicationController
   def index
     @trash = Trash.order(:name)
-    render 'index.html.erb'
+  end
+
+  def create
+    @trash = Trash.new(
+      name: params[:name],
+      accepted: params[:accepted],
+      alternative: params[:alternative],
+      mail: params[:mail],
+      address: params[:address],
+      phone: params[:phone],
+      website: params[:website],
+      details: params[:details],
+      )
+    if @trash.save
+      redirect_to @trash
+    else
+      render 'new.html.erb'
+    end
   end
   
   def show
     @trash = Trash.find(params[:id])
     puts @trash
+  end
+
+  def new
+    # @trash = Trash.new
   end
 end
